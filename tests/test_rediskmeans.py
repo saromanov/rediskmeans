6b34bcba92ef8027c5128440c6485bbde6055adc
Sergey Romanov <xxsmotur@gmail.com>
2015-06-18 22:05:44 +0500

    update test

--- tests/test_rediskmeans.py
+++ tests/test_rediskmeans.py
@@ -9,6 +9,7 @@ class TestClusteringWithTFIDF(unittest.TestCase):
         rkm.put("one2", "Kate told me about herself")
         rkm.put("one3", "Listen music")
         rkm.put("one4", "He told this")
-        result = rkm.apply(["one1", "two2", "one3", "one4"], tfidf=True, n_clusters=4)
-        self.assertEqual(len(result),2)
+        result = rkm.apply(["one1", "two2", "one3", "one4"], tfidf=True, n_clusters=2)
+        self.assertEqual(len(result),4)
+
 
