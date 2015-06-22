8c79bf28f6e7bbd6aaf9b3c7829be3f7119921c5
Sergey Romanov <xxsmotur@gmail.com>
2015-06-22 19:35:29 +0500

    reorganization of tests

--- tests/test_rediskmeans.py
+++ tests/test_rediskmeans.py
@@ -7,21 +7,7 @@ import os
 NAMES = ["n1", "n2", "n3", "n4", "n5", "n6", "n7", "n8", "n9", "n10", "n11"]
 
 
-class TestClusteringWithTFIDF(unittest.TestCase):
-    def setUp(self):
-        client = redis.Redis()
-        client.delete(NAMES)
-
-    def test_basic(self):
-        rkm = RedisKMeans()
-        rkm.put("one1", "music this great")
-        rkm.put("one2", "Kate told me about herself")
-        rkm.put("one3", "Listen music")
-        rkm.put("one4", "He told this")
-        result = rkm.apply(
-            ["one1", "two2", "one3", "one4"], tfidf=True, n_clusters=2)
-        self.assertEqual(len(result), 4)
-
+class TestClustering(unittest.TestCase):
     def test_with_numbers(self):
         rkm = RedisKMeans()
         rkm.put("n1", [0.8, 0.7, 0.6, 0.2, 0.1])
@@ -38,6 +24,23 @@ class TestClusteringWithTFIDF(unittest.TestCase):
         result = rkm.apply(NAMES, n_clusters=3)
         self.assertEqual(len(result), 11)
         self.assertEqual(set(result), set((0,1,2)))
+
+
+
+class TestClusteringWithTFIDF(unittest.TestCase):
+    def setUp(self):
+        client = redis.Redis()
+        client.delete(NAMES)
+
+    def test_basic(self):
+        rkm = RedisKMeans()
+        rkm.put("one1", "music this great")
+        rkm.put("one2", "Kate told me about herself")
+        rkm.put("one3", "Listen music")
+        rkm.put("one4", "He told this")
+        result = rkm.apply(
+            ["one1", "two2", "one3", "one4"], tfidf=True, n_clusters=2)
+        self.assertEqual(len(result), 4)
 
     def test_loading_keys_from_file(self):
         rkm = RedisKMeans()
